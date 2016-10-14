2b25a4a521ff4bf306beb7b4566fadd824d93da9
Артем Пачев < >
2016-10-15 00:31:23 +0300

    added 1 enemy

---
+++ Assets/_scripts/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemyController : MonoBehaviour {
+
+  public GameObject MiddleLeftSpawnEnemy;
+  public GameObject killMiddleLeftSpawnEnemy;
+
+
+  public GameObject MiddleRigthSpawnEnemy;
+  public GameObject killMiddleRigthSpawnEnemy;
+
+  public GameObject LeftSpawnEnemy;
+  public GameObject killLeftSpawnEnemy;
+
+
+  public GameObject RigthSpawnEnemy;
+  public GameObject killRigthSpawnEnemy;
+
+
+  public GameObject _enemy;
+
+  void Start()
+  {
+    _enemy = Instantiate(_enemy);
+  }
+
+  void Update()
+
+  {
+    _enemy.transform.position = Vector3.MoveTowards(_enemy.transform.position, killLeftSpawnEnemy.transform.position, Time.deltaTime * 2);
+  }
+
+}
